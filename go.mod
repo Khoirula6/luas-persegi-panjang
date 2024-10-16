ace8aa028b144bd0ea921eb5a6f1decdf927a3c3
Khoiru Anam <mbakcantik@MacBook-Pro-Khoiru.local>
2024-10-16 09:41:55 +0700

    struct luas persegi panjang

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Khoirula6/luas-persegi-panjang
+
+go 1.21.0
---
+++ rectangle/area.go
@@ -0,0 +1,10 @@
+package rectangle
+
+type Rectangle struct {
+	Length float32
+	Width  float32
+}
+
+func (rectangle *Rectangle) Area() float32 {
+	return rectangle.Length * rectangle.Width
+}
